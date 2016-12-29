8eeba1926d7256a209fc43609b95b8cf9185a5e2
Andrey Subbotin <subbotin@haulmont.com>
2016-12-29 15:42:26 +0400

    PL-6362 BI integration (add pentaho sample window)

---
+++ modules/web/src/com/haulmont/bidemo/web/bi/BIDemoWindow.java
@@ -0,0 +1,18 @@
+/*
+ * TODO Copyright
+ */
+
+package com.haulmont.bidemo.web.bi;
+
+import com.haulmont.cuba.gui.components.AbstractWindow;
+import com.haulmont.cuba.gui.components.VBoxLayout;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+public class BIDemoWindow extends AbstractWindow {
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+    }
+}
