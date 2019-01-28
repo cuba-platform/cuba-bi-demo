3aa11f1c85e332119a1426401b8313b0ae2062fc
Andrey Subbotin <subbotin@haulmont.com>
2019-01-28 18:40:27 +0400

    Migrate to 7.0

---
+++ modules/web/src/com/haulmont/bidemo/web/bi/BiDemoV7SaikuWindow.java
@@ -0,0 +1,10 @@
+package com.haulmont.bidemo.web.bi;
+
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("bidemoV7SaikuWindow")
+@UiDescriptor("bi-demo-v7-saiku-window.xml")
+public class BiDemoV7SaikuWindow extends Screen {
+}
